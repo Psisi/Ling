745dea6e4bcd9a4b6f55320aaa24cebe1619f3d6
Ling <1012574152@qq.com>
2021-10-09 12:05:00 +1300

    adjusted the format

--- planlist/domit.js
+++ planlist/domit.js
@@ -4,9 +4,9 @@ for(let i = 0; i < priceList.length; i++) {
     priceList[i].onmouseenter = function() {
         let price = priceList[i].innerHTML;
         if (price !== 'FREE') {
-        priceList[i].innerText = "$100 for the first month";  
-        priceList[i].style.color = 'red';
-        priceList[i].style.fontSize = '25PX';
+            priceList[i].innerText = "$100 for the first month";  
+            priceList[i].style.color = 'red';
+            priceList[i].style.fontSize = '25PX';
         };
     };  
 }
