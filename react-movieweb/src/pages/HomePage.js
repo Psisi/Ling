5a75e16fb85fbf9e07c305a75d6c08bf683a99cd
Ling <1012574152@qq.com>
2021-11-22 12:36:35 +1300

    simplified and formatted

--- react-movieweb/src/pages/HomePage.js
+++ react-movieweb/src/pages/HomePage.js
@@ -16,20 +16,20 @@ class HomePage extends Component {
         this.setState({movies: data? data.slice(0,10) : []});
     }
     render() {
+        const popularmoives = ['鱿鱼游戏', '扫黑风暴', '失控玩家', '绝命毒师', '速度与激情9', '全裸导演', '灵媒', '进击的巨人', '浴血黑帮', '黑寡妇']
+        const countriesname = ['新加坡', '泰国', '中国香港特别行政区', '马来西亚', '印度', '菲律宾', '捷克', '匈牙利', '南非', '澳大利亚', '英国', 
+            '以色列', '斯洛伐克', '俄罗斯', '冰岛', '立陶宛', '罗马尼亚', '墨西哥', '希腊', '阿根廷', '加拿大', '德国', '日本', '瑞士', '巴西', '土耳其',
+            '比利时', '意大利', '韩国', '西班牙', '荷兰', '哥伦比亚', '乌克兰', '美国', '瑞典', '法国', '葡萄牙', '波兰']
+        const movietypes = ['剧情', '喜剧', '惊悚', '动作', '爱情', '犯罪', '动画', '记录', '冒险', '恐怖', '科幻', '奇幻', '悬疑', '家庭', '传记', '短片', 
+            '音乐', '历史', '战争', '运动', '歌舞', '同性', '真人秀', '儿童', '西部', '脱口秀', '灾难', '情色', '古装', '武侠', '舞台艺术', '黑色电影', 
+            'Adult', 'Reality-TV', '戏曲', 'Game-show', '纪录片', '鬼怪', 'News', '荒诞']
         return(
             <div>
                 <Search/>
                 <div className={styles.popularmovies}>
-                    <span><img src={img}/>鱿鱼游戏</span>
-                    <span><img src={img}/>扫黑风暴</span>
-                    <span><img src={img}/>失控玩家</span>
-                    <span><img src={img}/>绝命毒师</span>
-                    <span><img src={img}/>速度与激情9</span>
-                    <span><img src={img}/>全裸导演</span>
-                    <span><img src={img}/>灵媒</span>
-                    <span><img src={img}/>进击的巨人</span>
-                    <span><img src={img}/>浴血黑帮</span>
-                    <span><img src={img}/>黑寡妇</span>  
+                    {popularmoives.map(movie => (
+                        <span key={movie}><img src={img}/>{movie}</span>
+                    ))}
                 </div>
                 <ButtonGroup onDataChange = {this.onDataChange} />
                 <div className = {styles['movie-list']}>
@@ -52,110 +52,36 @@ class HomePage extends Component {
                     <div className={styles['type-area']}>
                         <div className={styles.area}>地区</div>
                         <div className={styles['all-areas']}>
-                        <span className={styles.active}>所有地区</span>
-                        <span>新加坡</span>
-                        <span>泰国</span>
-                        <span>中国香港特别行政区</span>
-                        <span>马来西亚</span>
-                        <span>印度</span>
-                        <span>菲律宾</span>
-                        <span>捷克</span>
-                        <span>匈牙利</span>
-                        <span>南非</span>
-                        <span>澳大利亚</span>
-                        <span>英国</span>
-                        <span>以色列</span>
-                        <span>斯洛伐克</span>
-                        <span>俄罗斯</span>
-                        <span>冰岛</span>
-                        <span>立陶宛</span>
-                        <span>罗马尼亚</span>
-                        <span>墨西哥</span>
-                        <span>希腊</span>
-                        <span>阿根廷</span>
-                        <span>加拿大</span>
-                        <span>德国</span>
-                        <span>日本</span>
-                        <span>瑞士</span>
-                        <span>巴西</span>
-                        <span>土耳其</span>
-                        <span>比利时</span>
-                        <span>意大利</span>
-                        <span>韩国</span>
-                        <span>西班牙</span>
-                        <span>荷兰</span>
-                        <span>哥伦比亚</span>
-                        <span>乌克兰</span>
-                        <span>美国</span>
-                        <span>瑞典</span>
-                        <span>法国</span>
-                        <span>葡萄牙</span>
-                        <span>波兰</span>
-
+                            <span className={styles.active}>所有地区</span>
+                            {countriesname.map ( country => (
+                                <span key={country}>{country}</span>
+                            ))}
                         </div>
                     </div>
                     <div className={styles.types}>
                         <div className={styles.type}>类型</div>
                         <div className={styles['all-types']}>
-                        <span className={styles.active}>全部类型</span>
-                        <span>剧情</span>
-                        <span>喜剧</span>
-                        <span>惊悚</span>
-                        <span>动作</span>
-                        <span>爱情</span>
-                        <span>犯罪</span>
-                        <span>动画</span>
-                        <span>记录</span>
-                        <span>冒险</span>
-                        <span>恐怖</span>
-                        <span>科幻</span>
-                        <span>奇幻</span>
-                        <span>悬疑</span>
-                        <span>家庭</span>
-                        <span>传记</span>
-                        <span>短片</span>
-                        <span>音乐</span>
-                        <span>历史</span>
-                        <span>战争</span>
-                        <span>运动</span>
-                        <span>歌舞</span>
-                        <span>同性</span>
-                        <span>真人秀</span>
-                        <span>儿童</span>
-                        <span>西部</span>
-                        <span>脱口秀</span>
-                        <span>灾难</span>
-                        <span>情色</span>
-                        <span>古装</span>
-                        <span>武侠</span>
-                        <span>舞台艺术</span>
-                        <span>黑色电影</span>
-                        <span>Adult</span>
-                        <span>Reality-TV</span>
-                        <span>戏曲</span>
-                        <span>Game-show</span>
-                        <span>纪录片-</span>
-                        <span>鬼怪</span>
-                        <span>News</span>
-                        <span>荒诞</span>
-
+                            <span className={styles.active}>全部类型</span>
+                            {movietypes.map ( type => (
+                                <span key = {type}>{type}</span>
+                            ))}
                         </div>
                     </div>
                     <div className={styles['type-order']}>
                         <div className={styles.order}>排序</div>
                         <div className={styles['all-orders']}>
-                        <span className={styles.active}>近期上架</span>
-                        <span>评分</span>
-                        <span>新片</span>
-                        <span>老片</span>
+                            <span className={styles.active}>近期上架</span>
+                            <span>评分</span>
+                            <span>新片</span>
+                            <span>老片</span>
                         </div>
                     </div>
                     <div className={styles['type-subtitle']}>
                         <div className={styles.subtitle}>字幕</div>
                         <div className={styles['all-subtitiles']}>
-                        <span className={styles.active}>全部语言</span>
-                        <span>中文</span>
-                        <span>英文</span>
+                            <span className={styles.active}>全部语言</span>
+                            <span>中文</span>
+                            <span>英文</span>
                         </div>
                     </div>
                 </div>
